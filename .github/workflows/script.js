c6ffa1e8e00efbe21be37d0aaf2d0020a18f6940
Narius Wainwright <115397452+NariusandMontreal@users.noreply.github.com>
2025-04-29 07:55:42 -0400

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Array of floor sample image URLs (replace with your actual image paths)
     const sampleImages = [
-        'images/lvp_sample1.png',
-        'images/lvp_sample2.png',
-        'images/lvp_sample3.png',
+        'images/lvp_sample-1.png',
+        'images/lvp_sample-2.png',
+        'images/lvp_sample-3.png',
         // Add more image URLs here
     ];
 
